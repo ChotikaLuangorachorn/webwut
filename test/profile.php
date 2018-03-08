af8e1eefc4d844866f89def1e289e69c0b5b7fa7
jsice <w14chin@gmail.com>
2018-03-08 11:29:48 +0700

    profile page can show events user has went and events user's interested

--- test/profile.php
+++ test/profile.php
@@ -7,6 +7,7 @@
         if (array_key_exists('user', $_GET)) {
             $USER = $_GET['user'];
         }
+        $_GET['user'] = $USER;
         if ($USER == $_SESSION['ID']) {
             if ($ROLE == 'OR') {
                 header('location:/test/index.php');
@@ -30,8 +31,10 @@
         $statement->execute([$USER]);
         $info = $statement->fetch(PDO::FETCH_OBJ);
 
-        
-
+        $sql = "SELECT date, eventName, location, flag FROM event_attendant LEFT JOIN event USING (eventID) WHERE aID=?";
+        $statement = $conn->prepare($sql); 
+        $statement->execute([$USER]);
+        $events = $statement->fetchAll(PDO::FETCH_OBJ);
         if ($info === FALSE) {
             header('location:oops.html');
         }
@@ -47,9 +50,7 @@
     <link rel="stylesheet" href="profile.css">
 </head>
 <body>
-    <div class="banner">
-        
-    </div>
+    <?php include 'header.php'; ?>
     <div class="container content padding-top">
         <div class="row">
             <div class="col-12"><h1>Profile</h1><div>
@@ -77,12 +78,75 @@
                 </div>
             </div>
         </div>
+        <div class="row" id="attended-event">
+            <div class="col-lg-6 col-md-12" id="upcoming-events">
+                <div class="row"><div class="col-12 text-centered"><h1>Upcoming Events</h1></div></div>
+                <div class="row table-header row-with-centered-col">
+                    <div class="col-5 text-centered">Event Name</div>
+                    <div class="col-3 text-centered">Place</div>
+                    <div class="col-3 text-centered">Date</div>
+                </div>
+                <no-event class="row table-row row-with-centered-col">
+                    <div class="col-11 text-centered">No Event</div>
+                </no-event>
+            </div>
+            <div class="col-lg-6 col-md-12" id="past-events">
+                <div class="row"><div class="col-12 text-centered"><h1>Events You've Joined</h1></div></div>
+                <div class="row table-header row-with-centered-col">
+                    <div class="col-5 text-centered">Event Name</div>
+                    <div class="col-3 text-centered">Place</div>
+                    <div class="col-3 text-centered">Date</div>
+                </div>
+                <no-event class="row table-row row-with-centered-col">
+                    <div class="col-11 text-centered">No Event</div>
+                </no-event>
+            </div>
+        </div>
     <div>
+    
     
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    
+    <script>
+        var events = <?php if ($events !== FALSE) echo json_encode($events); else echo "[]"; ?>;
+        var past = [];
+        var soon = [];
+        var now = Date.now();
+        function parseSQLdate(sqlDate) {
+            sqlDate = sqlDate.replace(/:| /g,"-");
+            var YMDhms = sqlDate.split("-");
+            var jsDate = new Date();
+            jsDate.setFullYear(parseInt(YMDhms[0]), parseInt(YMDhms[1])-1, parseInt(YMDhms[2]));
+            jsDate.setHours(parseInt(YMDhms[3]), parseInt(YMDhms[4]), parseInt(YMDhms[5]), 0);
+            return jsDate
+        }
+        for (event of events) {
+            let date = parseSQLdate(event.date);
+            console.log(event);
+            if (date < now) {
+                past.push(event);
+                $("#past-events").append(`<div class="row table-row row-with-centered-col">
+                <div class="col-5 text-centered">`+event.eventName+`</div>
+                <div class="col-3 text-centered">`+event.location+`</div>
+                <div class="col-3 text-centered">`+date+`</div>
+                </div>`);
+            }
+            else {
+                soon.push(event);
+            }
+        }
+        if (past.length > 0) {
+            $('#past-events > no-event').hide();
+        } else {
+            $('#past-events > no-event').show();
+        }
+        if (soon.length > 0) {
+            $('#upcoming-events > no-event').hide();
+        } else {
+            $('#upcoming-events > no-event').show();
+        }
+    </script>
 </body>
 </html>
 
