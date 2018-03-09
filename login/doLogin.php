492b0b41ed4c777d345fab0e594e53459798097c
ohmygodkr <kampanat.ru@ku.th>
2018-03-09 13:50:33 +0700

    added: enter login

---
+++ login/doLogin.php
@@ -0,0 +1,47 @@
+<?php
+    //เรียกใช้ session
+    session_start();
+
+
+    if (array_key_exists("lg_username", $_POST) && array_key_exists("lg_password", $_POST)) {
+        $uid = $_POST['lg_username'];
+        $pass = $_POST['lg_password'];
+
+        include 'connectDB.php';
+        $sql = "SELECT * FROM user WHERE userID=? AND PASSWORD=?";
+        $statement = $conn->prepare($sql);
+        $statement->execute([$uid, $pass]);
+        $user = $statement->fetch(PDO::FETCH_OBJ);
+
+        if ($user !== FALSE) {
+            // echo "enter IF";
+            //set ตัวแปรใน session ไว้ว่าใคร log in อยู่ และเป็น role อะไร
+            $_SESSION["ID"] = $user->id;
+            $_SESSION["ROLE"] = $user->role;
+            session_write_close();
+            //เช็คว่ากด login มาจากหน้าไหนให้กลับไปหน้านั้น
+            //ถ้าไม่ได้บอกไว้ให้ไปหน้าแรกของแต่ละ Role
+            // if (array_key_exists("PREV", $_SESSION)) {
+            //     // header("location:".$_SESSION["PREV"]);
+            //     echo 'PREV';
+            // } else {
+            //     if ($user->role == 'AT') {
+            //         // header("location:index.php");
+            //         echo 'Welcome Attendant !';
+            //     }
+            //     if ($user->role == 'OR') {
+            //         // header("location:index.php");
+            //         echo 'Hello Organizer !!';
+            //     }
+            //     if ($user->role == 'AD') {
+            //         // header("location:admin.php");
+            //         echo 'Hi ! Admin';
+            //     }
+            // }
+        } else {
+            //login ไม่สำเร็จ show error
+            // header("location:login.php?e=1");
+            session_write_close();
+        }
+    }
+?>
---
+++ login/login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <!-- Bootstrap core CSS -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="loginStyle.css">
+</head>
+<body>
+    <?php
+        session_start();
+
+        if (array_key_exists("e", $_GET)) {
+            if ($_GET['e']==1) {
+                echo "<p>Incorrect Username and/or Password</p>";
+            } else if ($_GET['e']==2) {
+                echo "<p>Please login first.</p>";
+            }
+        }
+    ?>
+    <div class="text-center">
+        <div class="logo">login</div>
+        <!-- Main Form -->
+            <div class="login-form-1">
+                <form id="login-form" class="text-left" action="doLogin.php" method="post">
+                    <div class="login-form-main-message"></div>
+                    <div class="main-login-form">
+                        <div class="login-group">
+                            <div class="form-group">
+                                <label for="lg_username" class="sr-only">Username</label>
+                                <input type="text" class="form-control" id="lg_username" name="lg_username" placeholder="username">
+                            </div>
+                            <div class="form-group">
+                                <label for="lg_password" class="sr-only">Password</label>
+                                <input type="password" class="form-control" id="lg_password" name="lg_password" placeholder="password">
+                            </div>
+                            <div>
+                                <label class="btn login-button" for="submit_button"><i class="fa fa-chevron-right"><input type="submit" name="submit_button" id="submit_button" hidden></i></label>
+                            </div>
+                            <div class="etc-login-form">
+                                <p>new user? <a href="./registration.php">create new account</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- end:Main Form -->
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+</body>
+</html>
