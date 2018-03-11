91177656daabc609b9aad839aee79bb7e024b9ad
ohmygodkr <kampanat.ru@ku.th>
2018-03-11 11:28:18 +0700

    check username and email register

--- login/create_attendant.php
+++ login/create_attendant.php
@@ -13,14 +13,27 @@
     $role = 'AT';
 
 
+    //check username & email ไม่ซ้ำ
+    $query_check = "SELECT * FROM `organizer_info` LEFT JOIN `user` ON organizer_info.userID = user.id 
+        WHERE organizer_info.email='".$email."' AND user.userID='".$username."'";
+    $statement_check = $conn->query($query_check);
+    $row_check = $statement_check->fetchAll(PDO::FETCH_OBJ);
+    if (sizeof($row_check > 0)) {
+        echo "username and email has already !!";
+    } else {
     $query1 = "INSERT INTO `user` (`userID`,`password`,`role`) 
         VALUES ('".$username."','".$password."','".$role."')";
+    //insert username to DB
     $statement = $conn->exec($query1);
     echo " ------new Attendant------- ";
     echo $gender;
     $userID = $conn->lastInsertId();
     $query2 = "INSERT INTO `personal_info` (`userID`, `firstName`,`lastName`,`age`,`email`,`phoneNo`,`gender`) 
         VALUES ('$userID', '".$firstname."','".$lastname."','".$age."','".$email."','".$mobile_no."','".$gender."')";
+    //insert info to DB
     $statement = $conn->exec($query2);
-    echo " --------create new attendant-------- "
+    echo " --------create new attendant-------- ";
+    }
+    
+
 ?>
