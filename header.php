bc01aa23d5d37840635572b785715d914ee64d30
naanong <naanong.s@ku.th>
2018-03-12 18:56:13 +0700

    add new features: filter events

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
             echo "Welcome, ". $ID;
             ?>
             <a href="profile.php"><i class="material-icons" style="font-size:48px;color:#61b3cf">account_box</i></a>
-            <a href="personalMessage/"><i class="material-icons" style="font-size:48px;color:#df8a83">mail</i></a>
+            <a href="personalMessage.php"><i class="material-icons" style="font-size:48px;color:#df8a83">mail</i></a>
             <a href="logout.php"><button type="button" class="btn btn-light" id="btn-login" role="button">SIGN OUT</button></a>
             <?php
           } else {
